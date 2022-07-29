6c1327e16317e61e509903bd61ee77646bcf5f86
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-29 00:03:38 -0500

    indicando que sirva los estaticos de la applicacion de react

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,12 +2,13 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
+app.use(express.static('../app/build'))
 
-app.get('/', (_req, res) => {
-  res.send('Hello world!')
-})
+// app.get('/', (_req, res) => {
+//   res.send('Hello world!')
+// })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3002
 app.listen(PORT,()=>{
   console.log(`server is listening on ${PORT}`)
 })
