da3089d303aceb53a98d1a389385817845054d3e
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-30 14:11:54 -0500

    modificando componente App, creando usando hook necesarios para pedir y almacenar las naves

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import FormBuscar from './components/FormBuscar'
+import ListOfResult from './components/ListOfResults'
+import getNaves from './services/getNaves'
 
 function App () {
+  const [naves, setNaves] = useState([])
+  const [results, setResults] = useState([])
+  const [isCreating, setIsCreating] = useState(false)
+
+  useEffect(() => {
+    getNaves()
+      .then((naves) => {
+        setNaves(naves)
+        setResults(naves)
+      })
+      .catch((err) => console.error(err))
+  }, [])
+  const handleBuscar = (arg) => {
+    console.log(arg)
+    if (arg !== false) {
+      const resultsFiltered = naves.filter((e) => e.nombre.includes(arg))
+      setResults(resultsFiltered)
+    } else {
+      setResults(naves)
+    }
+  }
+  const hhh = () => {
+    setIsCreating(!isCreating)
+  }
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sofka Espacial</h1>
+      <div className='content'>
+        <div>
+          <FormBuscar handleBuscar={handleBuscar} />
+
+          <button
+            onClick={hhh}
+          >
+            {isCreating ? 'Cancelar' : 'Crear'}
+          </button>
+          {
+            isCreating && 'form'
+          }
+        </div>
+        <div className='content-list-naves'>
+          <ListOfResult results={results} />
+        </div>
+      </div>
     </div>
   )
 }
