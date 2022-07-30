c42696739515f0018d315477ded28ad5fb7d8ffc
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-30 14:33:00 -0500

    otorgando funcionalidad al form de buscar naves

--- app/src/components/FormBuscar/index.js
+++ app/src/components/FormBuscar/index.js
@@ -1,19 +1,33 @@
 import { useState } from 'react'
 
-function FormBuscar () {
+function FormBuscar ({ handleBuscar }) {
   const [buscar, setBuscar] = useState('')
   const handleChange = (e) => {
-    console.log(e.target)
-    setBuscar(e.target.value)
+    const { value } = e.target
+    setBuscar(value)
+    handleBuscar(value)
+    if (value === '') {
+      handleBuscar(false)
+    }
   }
   const handleSubmit = (e) => {
     e.preventDefault()
+    // if (buscar) {
+    //   handleBuscar(buscar)
+    // } else {
+    //   handleBuscar(false)
+    // }
   }
   return (
     <>
       <h2>Buscar Nave</h2>
       <form onSubmit={handleSubmit}>
-        <input type='text' value={buscar} onChange={handleChange} />
+        <input
+          type='text'
+          value={buscar}
+          onChange={handleChange}
+          placeholder='ingrese termino a buscar'
+        />
         <button>Buscar</button>
       </form>
     </>
