0831c77d5c8a79a9d84020b69be6118ae2a6e4e6
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-29 20:05:58 -0500

    creando esquemas para todas las naves

--- api/src/models/naves.ts
+++ api/src/models/naves.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-import { INaveLanzadera } from "../type"
+import { INaveLanzadera, INaveNoTripulada, INaveTripulada } from "../type"
 const lanzaderaSchema = new mongoose.Schema<INaveLanzadera>({ // creando esquema para la nave lanzadera
   nombre: String,
   peso: Number,
@@ -7,11 +7,34 @@ const lanzaderaSchema = new mongoose.Schema<INaveLanzadera>({ // creando esquema
   tipo: String,
   combustible: String,
   velocidadMax: Number,
+  procedencia: String,
   capCarga: Number,
+  cantPropulsores: String
 })
 
 export const modelNaveLanzadera = mongoose.model('NaveLanzaderaDB',lanzaderaSchema)
 
-// const tripuladaShema = new mongoose.Schema({
+const tripuladaShema = new mongoose.Schema<INaveTripulada>({
+  nombre: String,
+  peso: Number,
+  empuje: Number,
+  tipo: String,
+  combustible: String,
+  velocidadMax: Number,
+  procedencia: String,
+  capPasajeros: Number
+})
+export const modelNaveTripulada = mongoose.model('NaveTripuladaDB', tripuladaShema)
 
-// })
+const noTripuladaSchema = new mongoose.Schema<INaveNoTripulada>({
+  nombre: String,
+  peso: Number,
+  empuje: Number,
+  tipo: String,
+  combustible: String,
+  velocidadMax: Number,
+  procedencia: String,
+  altitud: Number,
+  puedeAterrizar: Boolean
+})
+export const modelNaveNoTripulada = mongoose.model('NaveNoTripuladaDB', noTripuladaSchema)
