64f5cf931206c014a716ea573d421f0e88de3f85
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-30 14:01:17 -0500

    completando la informacion de las naves a mostrar en las tarjetas

--- app/src/components/CardNave/index.js
+++ app/src/components/CardNave/index.js
@@ -1,9 +1,15 @@
-function CardNave ({ id, nombre, tipo }) {
-  console.log(id)
+import './cardNave.css'
+function CardNave ({ _id, nombre, peso, empuje, tipo, combustible, velocidadMax, procedencia }) {
   return (
-    <div onClick={() => console.log(id)}>
+    <div className='card-nave' onClick={() => console.log(_id)}>
       <h3>{nombre}</h3>
-      <p>{tipo}</p>
+      <p><strong>nave de tipo: </strong>{tipo}</p>
+      <p><strong>peso: </strong>{peso}</p>
+      <p>con {empuje} de empuje</p>
+      <p>usa {combustible} como combustible</p>
+      <p>alcanza una velocidad de {velocidadMax} km/h</p>
+      <p><strong>procedente de: </strong>{procedencia}</p>
+
     </div>
   )
 }
--- app/src/components/ListOfResults/index.js
+++ app/src/components/ListOfResults/index.js
@@ -1,14 +1,19 @@
 import CardNave from '../CardNave'
 function ListOfResult ({ results }) {
-  console.log(results)
+  // console.log(results)
   return (
     <>
-      {results.map(({ id, nombre, tipo }) => (
+      {results.map(({ _id, nombre, peso, empuje, tipo, combustible, velocidadMax, procedencia }) => (
         <CardNave
-          key={id}
-          id={id}
+          key={_id}
+          id={_id}
           nombre={nombre}
           tipo={tipo}
+          peso={peso}
+          empuje={empuje}
+          combustible={combustible}
+          velocidadMax={velocidadMax}
+          procedencia={procedencia}
         />
       ))}
     </>
